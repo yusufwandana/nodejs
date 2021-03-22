4f27387d082c96b97385aa2f6e7f9e03a58df087
yusufwandana <yusuf.wandana1@gmail.com>
2021-03-22 09:07:45 +0700

    #5 - Object Reference

---
+++ object_reference/script.js
@@ -0,0 +1,11 @@
+var obj1 = {
+    "nama"  :   "Ganteng kalem",
+    "umur"  :   13
+}
+
+var obj2 = obj1;
+obj2.umur = 18;
+
+console.log(obj1);
+console.log("- - - - - - - -");
+console.log(obj2);
